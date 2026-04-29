7120f8b00fe8f5cde25e237db0fafa53cf5df098
Shreyas Poojary <shreyas.poojary@gmail.com>
2026-04-29 10:14:35 +0530

    24-9 deployment

--- backend/server.js
+++ backend/server.js
@@ -16,13 +16,13 @@ connectDB();
 
 const app=express();
 
-app.use(cors({
-  origin: ["https://inventrack-silk.vercel.app"],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: [process.env.CLIENT_URL],
+    credentials: true
+  })
+);
 app.use(express.json());
-
-console.log("API KEY:", process.env.CLOUDINARY_API_KEY);
 app.use("/api/suppliers", supplierRoutes);
 app.use("/api/products",productRoutes);
 app.use("/api/sales", saleRoutes);
--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://inventory-and-sales-analytics.onrender.com/api"
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 // attach token automatically
